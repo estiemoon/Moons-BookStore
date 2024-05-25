412736e3bafbda6a7c577c8400d5f71a32b52262
estie <harin0375@gmail.com>
2024-05-25 21:46:35 +0900

    FEAT: 도서 API 기능 추가
    도서 신간조회 or 카테고리별 조회 기능 추가
    도서 페이징 기능 추가

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,46 +2,49 @@ const conn = require('../mariadb');
 const { StatusCodes} = require('http-status-codes');
 
 
+//카테고리별 Or 신간별 전체도서조회
 const allBooks = (req,res)=> {
-
-    let {category_id} = req.query
-
-    if (category_id){
-        category_id = parseInt(category_id)
-
-        let sql  = 'SELECT * FROM books WHERE category_id = ?'
-        conn.query(sql,category_id,
-            (err,result)=>{
-                if(err){
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
-                if (result.length){
-                    return res.status(StatusCodes.OK).json(result);
-                } else{
-                    return res.status(StatusCodes.NOT_FOUND).end();
-                }
-        })
-    } else {
-        let sql = 'SELECT * FROM books';
-        conn.query(sql,
-            (err,result)=>{
-                if(err){
-                    console.log(err);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
+    let {category_id, news,limit,currentPage} = req.query;
+    category_id = parseInt(category_id)
+    news = news == 'true' ? true : false ;
+    let offset = limit * (currentPage-1)
     
-                return res.status(StatusCodes.OK).json(result);
-    
-        })
+    let sql = `SELECT * FROM books`; 
+    let values = [];
+ 
+    if (category_id && news) {
+        sql += ` WHERE category_id = ? AND datediff(curdate(), pub_date) < 31`;
+        values.push(category_id);
+    }else if (category_id) {
+        sql += ` WHERE category_id = ?`;
+        values.push(category_id);
+    }else if (news) {
+        sql += ` WHERE datediff(curdate(), pub_date) < 31`;
     }
+    sql += ' LIMIT ? OFFSET ?'
+    values.push(parseInt(limit), offset);
+
+    conn.query(sql,values,
+        (err,result)=>{
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            if (result.length){
+                return res.status(StatusCodes.OK).json(result);
+            } else{
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+    })
 };
 
 const eachBook = (req,res) => {
     let {id} = req.params;
     id = parseInt(id);
     
-    let sql = 'SELECT * FROM books WHERE id = ?';
+    let sql = `SELECT * FROM books LEFT 
+               JOIN categories ON categories.id = books.category_id 
+               WHERE books.id = ?`;
     conn.query(sql,id,
         (err,result)=>{
             if(err){
@@ -55,11 +58,7 @@ const eachBook = (req,res) => {
             }
 
     })
-
-    
 };
-
-
 
 
 module.exports = {
